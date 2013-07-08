0e8780ecc1cc6c2bcd2b338778e17c250478ad81
laurabreiman <lauracle@mit.edu>
2013-07-08 13:16:10 -0400

    framework

---
+++ markov.js
@@ -0,0 +1,81 @@
+var markovChain = (function() {
+    
+    var exports = {};
+    
+////////////////////////////////// global variables 
+    
+    //d3 chart components
+    var chart;
+    
+    var xMin = -10;
+    var xMax = 10;
+    var yMin = -10;
+    var yMax = 10;
+    
+    var outer_height = 300;
+    var outer_width = 300;
+
+    var margin = { top: 20, right: 20, bottom: 20, left: 20 }
+    var chart_width = outer_width - margin.left - margin.right;
+    var chart_height = outer_height -margin.top - margin.bottom;
+    
+    var x_scale = d3.scale.linear().domain([xMin,xMax]).range([0,chart_width]);
+    var y_scale = d3.scale.linear().domain([yMin,yMax]).range([chart_height,0]);
+    
+////////////////////////////////// helper functions    
+    
+    //rounds a number (number) to the specified amount of decimal points (decimals)
+    function round_number(number,decimals){
+        return Math.round(number*Math.pow(10,decimals))/Math.pow(10,decimals)
+    }
+    
+    //creates a range of values from start to stop in step sized increments
+    function range(start, stop, step){
+    if (typeof stop=='undefined'){
+        // one param defined
+        stop = start;
+        start = 0;
+    };
+    if (typeof step=='undefined'){
+        step = 1;
+    };
+    if ((step>0 && start>=stop) || (step<0 && start<=stop)){
+        return [];
+    };
+    var result = [];
+    for (var i=start; step>0 ? i<stop : i>stop; i+=step){
+        result.push(i);
+    };
+    return result;
+};
+/////////////////////////////////// set up div functions
+    
+    funciton Model(){
+    }
+    
+    function Controller(){
+    }
+    
+    function View(){
+    }
+    
+    //setup main structure of app
+    function setup(div) {
+
+        var model = Model();
+        var controller = Controller(model);
+        var view = View(div, model, controller);
+        
+    }; 
+    
+    exports.setup = setup;
+    exports.model = Model;
+    exports.view = View;
+    exports.controller = Controller;
+
+    return exports;
+}());
+
+$(document).ready(function() {
+    markovChain.setup($('.markov'));
+});
