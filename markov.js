e12d41f624e40b1d6313de71fbdc35aeb98f5d6d
So yeun Cho <socho@mmit.edu>
2013-07-19 15:14:55 -0400

    added equation

--- markov.js
+++ markov.js
@@ -370,6 +370,22 @@ var markovChain = (function() {
             result[answers.length+1] = NaN;
            return result;
         }
+
+        /*
+        function that takes in an array of length one and an observation string
+        returns a value of 0 (wrong) or 1 (right), based on the probability of Obs=obs
+        */
+        function checkSum(answer,observation){
+            console.log(answer[0]);
+            var correctAns = model.prob_OnS()[observation];
+            var sum = 0;
+            for (var i = 0; i < Object.keys(correctAns).length; i++){
+                sum += correctAns[i];
+            }
+            // console.log(sum);
+            if (answer[0].toFixed(3) == sum.toFixed(3)) {return 1;}
+            else {return 0;}
+        }
         
         /*
         function that takes in an array of probabilites and an observation string
@@ -393,7 +409,7 @@ var markovChain = (function() {
             return result;
         }
         
-        return {checkAnswers: checkAnswers, checkOgS: checkOgS, checkOnS: checkOnS, checkObs: checkObs};
+        return {checkAnswers: checkAnswers, checkOgS: checkOgS, checkOnS: checkOnS, checkSum: checkSum, checkObs: checkObs};
 
     }
     
@@ -555,7 +571,7 @@ var markovChain = (function() {
                                     firstDiv.append(firstDivBtn);
                                     $('.firstDivBtn').on("click", function(){
                                         firstDiv.remove();
-                                        updateFirstInputRow();
+                                        updateFirstInputRow("first");
                                     });
                                 });
                                 firstDiv.fadeIn('slow');
@@ -686,6 +702,10 @@ var markovChain = (function() {
         }
         
         function nextState(){
+            for (var i = 3; i<=101; i++){
+                $("#MathJax-Span-"+i).css("color","black");                
+            }            
+
             var points = model.get_current_state_array();
             var pointdict = model.get_current_state();
             var numpoints = points.length;
@@ -704,7 +724,7 @@ var markovChain = (function() {
                 $(this).closest('.row-fluid').remove();
                 $('.num-label'+state).remove(); $('.first-prob'+state).remove(); //to remove the duplicate
                 firstupdate(state);
-                updateFirstInputRow(state);
+                updateFirstInputRow("rest");
             })
         }
         
@@ -828,7 +848,14 @@ var markovChain = (function() {
                 .text(function(d) { return round_number(d,4); });
         }
         
-        function updateFirstInputRow(){
+        function updateFirstInputRow(status){
+            for (var i = 3; i<=101; i++){
+                $("#MathJax-Span-"+i).css("color","black");                
+            }
+            for (var i = 71; i<=76; i++){
+                $("#MathJax-Span-"+i).css("color","red");                
+            }
+
             $('.side-labels').append("<label class='second-prob"+state+"'>P(S<sub>"+(state+1)+"</sub>=s)</label>");
             $('.second-prob'+state).append('<i class="icon icon-question-sign second-prob-icon" rel="tooltip"></i>');
             $(".span7").append("<div class = 'row-fluid'><div class = 'first-row"+state+" textbox-row input-row'></div></div>");
@@ -880,8 +907,8 @@ var markovChain = (function() {
                 if(results[results.length-2] == 1){
                     $(this).remove(); $('.trans_feedback').remove();
                     $('.check-row'+state).append("<button class='btn btn-small observation'>Make Observation</button>");
-                    
-                    $('.observation').on("click",makeObservation);
+                    if (status == "first"){$('.observation').on("click",makeObservationFirstOnly);}
+                    else {$('.observation').on("click",makeObservation);}
                 }
             });
             $(document).keypress(function(event){
@@ -898,12 +925,17 @@ var markovChain = (function() {
         function checkView(indexOfCheck,observation){
             var answers = [];
             var num_entries = model.get_current_state_array().length;
-                
-            for(var i=0; i<num_entries; i++){
-                var input = $('.input-row .'+i).val()
+            
+            if (indexOfCheck == 3){
+                var input = $('.input-row .'+0).val();
                 answers.push(calculator.evaluate(calculator.parse(input)));
+            }   
+            else { 
+                for(var i=0; i<num_entries; i++){
+                    var input = $('.input-row .'+i).val()
+                    answers.push(calculator.evaluate(calculator.parse(input)));
+                }
             }
-
             if(indexOfCheck == 0){
                 transitionBottom(state, answers);
                 var results = controller.checkAnswers(answers);
@@ -914,29 +946,59 @@ var markovChain = (function() {
             else if(indexOfCheck == 2){
                 var results = controller.checkOnS(answers,observation);
             }
-            else{// if(indexOfCheck == 3){
+            else if(indexOfCheck == 3){
+                var results = controller.checkSum(answers,observation);
+            }
+            else{// if(indexOfCheck == 4){
                 transitionBottom(state, answers);
                 var results = controller.checkObs(answers,observation);
             } 
             
             $('.input-row .icon').remove();
             
-            for(var i = 0; i<results.length-2 ; i++){
-                if(results[i] == "right"){
-                    $('.input-row .'+i).after('<i class="icon icon-large icon-ok" id="icon'+i+'"></i>');
-                    $(".input-row #icon"+i).offset({left: $('.input-row .'+i).offset().left + parseInt($('.input-row .'+i).css("width"))+5});
-                    $('.input-row .'+i).val(answers[i]);
-                    $('.input-row .'+i).attr("disabled",true);
+            if (indexOfCheck == 3){
+                if (results == 1){
+                    $('.input-row .'+0).after('<i class="icon icon-large icon-ok" id="icon'+0+'"></i>');
+                    $(".input-row #icon"+0).offset({left: $('.input-row .'+0).offset().left + parseInt($('.input-row .'+0).css("width"))+5});
+                    $('.input-row .'+0).val(answers[0]);
+                    $('.input-row .'+0).attr("disabled",true);                    
                 }
                 else{
-                    $('.input-row .'+i).after('<i class="icon icon-large icon-remove" id="icon'+i+'"></i>');
-                    $(".input-row #icon"+i).offset({left: $('.input-row .'+i).offset().left + parseInt($('.input-row .'+i).css("width"))+5});
+                    $('.input-row .'+0).after('<i class="icon icon-large icon-remove" id="icon'+0+'"></i>');
+                    $(".input-row #icon"+0).offset({left: $('.input-row .'+0).offset().left + parseInt($('.input-row .'+0).css("width"))+5});
+                }
+            }
+            else {
+                for(var i = 0; i<results.length-2 ; i++){
+                    if(results[i] == "right"){
+                        $('.input-row .'+i).after('<i class="icon icon-large icon-ok" id="icon'+i+'"></i>');
+                        $(".input-row #icon"+i).offset({left: $('.input-row .'+i).offset().left + parseInt($('.input-row .'+i).css("width"))+5});
+                        $('.input-row .'+i).val(answers[i]);
+                        $('.input-row .'+i).attr("disabled",true);
+                    }
+                    else{
+                        $('.input-row .'+i).after('<i class="icon icon-large icon-remove" id="icon'+i+'"></i>');
+                        $(".input-row #icon"+i).offset({left: $('.input-row .'+i).offset().left + parseInt($('.input-row .'+i).css("width"))+5});
+                    }
                 }
             }
             return results;
         }
-                
+        
+        /*
+            function that makes an observation and displays the observation to the user through text
+            and a visual display of the observed block
+            then triggers next row of calculations
+        */
         function makeObservation(){
+            $('.observation').remove();
+            var observation = model.make_obs();
+            $(".check-row"+state).after("<div class='row-fluid obs-label obs-label"+state+"'>You observe a <span style='color:"+observation+"'>"+observation+"</span> block!</div>");
+            $(".help-text").html("You observe a <span style='color:"+observation+"'>"+observation+"</span> block!");
+            observeBlock(observation);
+            displayOgSInputRow(observation);
+        }        
+        function makeObservationFirstOnly(){
             $('.observation').remove();
             var observation = model.make_obs();
             $(".check-row"+state).after("<div class='row-fluid obs-label obs-label"+state+"'>You observe a <span style='color:"+observation+"'>"+observation+"</span> block!<div");
@@ -1017,7 +1079,9 @@ var markovChain = (function() {
         }
         
         function displayOnSInputRow(observation){
-            $('.equation').css("color","blakc");
+            for (var i = 3; i<=101; i++){
+                $("#MathJax-Span-"+i).css("color","black");                
+            }
             for (var i = 24; i<=37; i++){
                 $("#MathJax-Span-"+i).css("color","red");                
             }
@@ -1030,7 +1094,7 @@ var markovChain = (function() {
             $('.side-labels').append("<div class='ons-label"+state+"'>P(O="+observation+",S<sub>"+(state+1)+"</sub>=s)</div>");
             $('.ons-label'+state).append('<i class="icon icon-question-sign ons-label-icon" rel="tooltip"></i>');
 
-            $('.ons-label-icon').attr("title","= P(S<sub>"+(state+1)+"</sub>=s) \xD7 P(O="+observation+"|S<sub>"+(state+1)+"</sub>=s)");
+            $('.ons-label-icon').attr("title","= P(O="+observation+"|S<sub>"+(state+1)+"</sub>=s) \xD7 P(S<sub>"+(state+1)+"</sub>=s)");
             $('.ons-label-icon').tooltip({placement:'bottom', html:true});
             
             var num_entries = model.get_current_state_array().length;
@@ -1058,23 +1122,63 @@ var markovChain = (function() {
     
                 if(results[results.length-2] == 1){
                     $(this).remove();
-                    displayNormInputRow(observation);
+                    displaySumRow(observation);
                 }
             });
         }
 
         function displaySumRow(observation){
+            for (var i = 3; i<=101; i++){
+                $("#MathJax-Span-"+i).css("color","black");                
+            }
+            for (var i = 39; i<=48; i++){
+                $("#MathJax-Span-"+i).css("color","red");                
+            }
+            for (var i = 78; i<=101; i++){
+                $("#MathJax-Span-"+i).css("color","red");                                
+            }
             $(".check-row"+state).remove();
+            $(".span7").append("<div class='row-fluid'><div class ='textbox-row sum-row"+state+"'></div></div>");
+ 
+            $('.side-labels').append("<div class='sum-label"+state+"'>P(O="+observation+")</div>");
+            $('.sum-label'+state).append('<i class="icon icon-question-sign sum-label-icon" rel="tooltip"></i>');
+            $('.sum-label-icon').attr("title","&Sigma; P(O="+observation+",S<sub>"+(state+1)+"</sub>=s)");
+            $('.sum-label-icon').tooltip({placement:'bottom', html:true});
+
+            var num_entries = model.get_current_state_array().length;
             
+            $('.sum-row'+state).append("<input class='obs-entry "+0+"' placeholder='P(O="+observation+"'>");
+            $('.sum-row'+state+' .'+0+'').offset({left: $(".sum-row"+state).offset().left + chart_width/2});
+            $('.obs-entry').css("width",""+(10-num_entries/3)+"%");
+
+            $('.sum-label'+state).offset({top: $(".sum-row"+state).offset().top});
+            
+            $('.sum-row'+state).after("<div class='row-fluid check-row check-row"+state+"'><button class='btn btn-small check'>Check</button></div>");
+            
+            $('.input-row').removeClass('input-row');
+            $('.sum-row'+state).addClass('input-row');
+            $('.check').on('click',function(){
+                var result = checkView(3,observation);
+                if(result == 1){
+                    $(this).remove();
+                    displayNormInputRow(observation);
+                }
+            }); 
         }
         
         function displayNormInputRow(observation){
+            for (var i = 3; i<=101; i++){
+                $("#MathJax-Span-"+i).css("color","black");                
+            }
+            for (var i = 3; i<=18; i++){
+                $("#MathJax-Span-"+i).css("color","red");                
+            }
             $(".check-row"+state).remove();
             $(".span7").append("<div class='row-fluid'><div class ='textbox-row input-norm-row"+state+"'></div></div>");
             
             $('.side-labels').append("<div class='norm-label"+state+"'>P(S<sub>"+(state+1)+"</sub>=s|O="+observation+")</div>");
             $('.norm-label'+state).append('<i class="icon icon-question-sign norm-label-icon" rel="tooltip"></i>');
-            $('.norm-label-icon').attr("title","=  P(O="+observation+",S<sub>"+(state+1)+"</sub>=s)<br>\xF7 &Sigma; P(O="+observation+",S<sub>"+(state+1)+"</sub>=state)");
+            $('.norm-label-icon').attr("title","=  P(O="+observation+",S<sub>"+(state+1)+"</sub>=s)<br>\xF7 P(O="+observation+")");
             $('.norm-label-icon').tooltip({placement:'bottom', html:true});
             
             var num_entries = model.get_current_state_array().length;
@@ -1098,7 +1202,7 @@ var markovChain = (function() {
             
             $('.check').on('click',function(){
                 
-                var results = checkView(3,observation);
+                var results = checkView(4,observation);
                 if(results[results.length-1] == "sum_error"){
                     $('.obs_feedback').remove();
                     $('.check-row'+state).append('<div class="obs_feedback">should sum to 1.</div>');
